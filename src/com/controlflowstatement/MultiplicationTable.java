4b8c27bf036e50ca6ae186e67b9866db19a2c1cc
payal-yadav23 <payal.p.yadav45@gmail.com>
2025-08-11 19:50:58 +0530

    added files

---
+++ src/com/controlflowstatement/MultiplicationTable.java
@@ -0,0 +1,19 @@
+package com.controlflowstatement;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		int num = sc.nextInt();
+		int i = 1;
+
+		do {
+			System.out.println(num + " x " + i + " = " + (num * i));
+			i++;
+		} while (i <= 10);
+
+		sc.close();
+	}
+}
